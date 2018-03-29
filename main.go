5ec864786bb3b3c771334f4e25998c4db7165a87
Maxime Vidori <maxime.vidori@gmail.com>
2018-03-29 19:57:15 +0200

    Modify a bit the hmtl layout, add some css

--- main.go
+++ main.go
@@ -9,6 +9,7 @@ import (
 	"net/http"
 	"os"
 	"path"
+	"path/filepath"
 	"strings"
 	"text/template"
 
@@ -22,11 +23,47 @@ const (
 		<meta charset="utf-8">
 		<link href="/static/gfm.css" media="all" rel="stylesheet" type="text/css"/>
 		<link href="//cdnjs.cloudflare.com/ajax/libs/octicons/2.1.2/octicons.css" media="all" rel="stylesheet" type="text/css"/>
+		<style>
+			.boxed-group {
+				max-width: 980px;
+				margin: auto;
+			}
+			.boxed-group > h3 {
+				padding: 9px 10px 10px;
+				margin: 0;
+				font-size: 14px;
+				line-height: 17px;
+				background-color: #f6f8fa;
+				border: 1px solid #ddd;
+				border-bottom: 0;
+				border-radius: 3px 3px 0 0;
+			}
+			.entry-content > :first-child {
+				margin-top: 0 !important;
+			}
+			.entry-content {
+				padding: 30px;
+				border: 1px solid #ddd;
+				border-radius: 0 0 3px 3px;
+			}
+			.octicon {
+				vertical-align: text-bottom;
+			}
+		</style>
 	</head>
-	<body>
-		<article class="markdown-body entry-content" style="padding: 30px;">
-		{{.Content}}
-		</article>
+	<body class="markdown-body">
+		<div class="boxed-group">
+		<h3>
+			<svg class="octicon octicon-book" viewBox="0 0 16 16" version="1.1" width="16" height="16" aria-hidden="true">
+				<path fill-rule="evenodd" d="M3 5h4v1H3V5zm0 3h4V7H3v1zm0 2h4V9H3v1zm11-5h-4v1h4V5zm0 2h-4v1h4V7zm0 2h-4v1h4V9zm2-6v9c0 .55-.45 1-1 1H9.5l-1 1-1-1H2c-.55 0-1-.45-1-1V3c0-.55.45-1 1-1h5.5l1 1 1-1H15c.55 0 1 .45 1 1zm-8 .5L7.5 3H2v9h6V3.5zm7-.5H9.5l-.5.5V12h6V3z">
+				</path>
+			</svg>
+			{{.File}}
+			</h3>
+			<article class="markdown-body entry-content">
+      {{.Content}}
+      </article>
+    </div>
 	</body>
 </html>`
 )
@@ -38,6 +75,7 @@ var (
 
 type (
 	ResponseWriter struct{ http.ResponseWriter }
+	dict           map[string]interface{}
 )
 
 func (rw *ResponseWriter) WriteHeader(statusCode int) {
@@ -53,7 +91,10 @@ func render(w http.ResponseWriter, path string) {
 	std.Printf("Rendering %s", path)
 	content, err := ioutil.ReadFile(path)
 	if err == nil {
-		err = t.Execute(w, struct{ Content string }{string(md.Markdown(content))})
+		err = t.Execute(w, dict{
+			"File":    filepath.Base(path),
+			"Content": string(md.Markdown(content)),
+		})
 	}
 	errorHandling(w, err)
 }
@@ -64,14 +105,18 @@ func errorHandling(w http.ResponseWriter, err error) {
 		return
 	case os.IsNotExist(err):
 		w.WriteHeader(http.StatusNotFound)
-		data := map[string]interface{}{"Content": "<h1>404: Page Not Found</h1>"}
+		data := dict{
+			"File":    "File Not Found",
+			"Content": "<h1>404: Page Not Found</h1>",
+		}
 
 		if err = t.Execute(w, data); err == nil {
 			return
 		}
 		fallthrough
 	default:
-		data := map[string]interface{}{
+		data := dict{
+			"File":    "Internal Server Error",
 			"Content": fmt.Sprintf("<h1>500 Internal Server Error: %s</h1>", err),
 		}
 		w.WriteHeader(http.StatusInternalServerError)
